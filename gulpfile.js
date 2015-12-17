377e77840d6b89644c5df394bb167ee0a5773c75
sugarshin <shinsugar@gmail.com>
2015-12-18 01:02:00 +0900

    update

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,7 @@ const webpack = require('webpack');
 const runSequence = require('run-sequence');
 const browserSync = require('browser-sync');
 const del = require('del');
+const vinylPaths = require('vinyl-paths');
 const syntax = require('postcss-scss');
 const webpackConfig = require('./webpack.config');
 
@@ -26,43 +27,6 @@ const production = process.env.NODE_ENV === 'production';
 $.sprockets.declare(assetsPaths, destPath);
 
 gulp.task('clean', () => del(destPath));
-
-gulp.task('build:image', () => {
-  return gulp.src([`${assetsPaths.app}/images/**/*.{jpg,gif,png}`])
-    .pipe($.if(production, $.sprockets.precompile()))
-    .pipe(gulp.dest(destPath));
-});
-
-gulp.task('build:js', () => {
-  return gulp.src([`${destPath}/*.js`])
-    .pipe($.sprockets.precompile())
-    .pipe(gulp.dest(destPath));
-});
-
-gulp.task('build:scss', () => {
-  return gulp.src([`${assetsPaths.app}/stylesheets/entries/*.scss`])
-    .pipe($.cached('scss'))
-    .pipe($.postcss(postcssProcessors, { syntax }))
-    .pipe($.sprockets.scss({ precompile: production }))
-    .pipe($.if(production, $.sprockets.precompile()))
-    .pipe(gulp.dest(destPath))
-});
-
-gulp.task('server', () => {
-  browserSync({
-    files: [
-      './app/views/**/*.html.*',
-      `${destPath}/*.js`,
-      `${destPath}/*.css`
-    ],
-    proxy: {
-      target: 'localhost:3000'
-    },
-    ui: false,
-    notify: false,
-    ghostMode: false
-  });
-});
 
 const devWebpack = webpack(Object.assign({}, webpackConfig, {
   devtool: 'hidden-source-map',
@@ -92,16 +56,65 @@ gulp.task('webpack:build', cb => {
   });
 });
 
-gulp.task('default', cb => {
+gulp.task('build:image', () => {
+  return gulp.src([`${assetsPaths.app}/images/**/*.{jpg,gif,png}`])
+    .pipe($.if(production, $.sprockets.precompile()))
+    .pipe(gulp.dest(destPath));
+});
+
+// webpackからのstreamで（webpack-stream利用）vinylオブジェクトの値が正しくなく
+// sprockets.precompileでエラーになるので
+// 一旦`webpack:build`でpublic/assetsに吐いたあとでprecompile
+gulp.task('build:js', () => {
+  return new Promise((resolve, reject) => {
+    const vPaths = vinylPaths();
+    gulp.src([`${destPath}/*.js`])
+      .pipe(vPaths)
+      .pipe($.sprockets.precompile())
+      .pipe(gulp.dest(destPath))
+      .on('end', () => del(vPaths.paths).then(resolve).catch(reject));
+  });
+});
+
+gulp.task('build:scss', () => {
+  return gulp.src([`${assetsPaths.app}/stylesheets/entries/*.scss`])
+    .pipe($.cached('scss'))
+    .pipe($.postcss(postcssProcessors, { syntax }))
+    .pipe($.sprockets.scss({ precompile: production }))
+    .pipe($.if(production, $.minifyCss()))
+    .pipe($.if(production, $.sprockets.precompile()))
+    .pipe(gulp.dest(destPath))
+});
+
+gulp.task('preserver', cb => {
   runSequence(
     'clean',
-    'build:image',
-    ['webpack:dev', 'build:scss'],
+    ['build:image', 'webpack:dev', 'build:scss'],
     cb
   );
 });
 
-gulp.task('watch', ['default', 'server'], () => {
+gulp.task('server', ['preserver'], () => {
+  browserSync({
+    files: [
+      './app/views/**/*.html.*',
+      `${destPath}/*.js`,
+      `${destPath}/*.css`
+    ],
+    proxy: {
+      target: 'localhost:3000'
+    },
+    ui: false,
+    notify: false,
+    ghostMode: false
+  });
+});
+
+gulp.task('watch', ['server'], () => {
+  gulp.watch([`${assetsPaths.app}/stylesheets/**/*.scss`], ['build:scss'])
+    .on('change', ev => {
+      console.log(`File ${ev.path} was ${ev.type}, running build task...`);
+    });
   gulp.watch([`${assetsPaths.app}/javascripts/**/*.{js,jsx,ts,tsx}`], ['webpack:dev'])
     .on('change', ev => {
       console.log(`File ${ev.path} was ${ev.type}, running build task...`);
